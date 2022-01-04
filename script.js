6815096cf5dbd4080bdbb3dd794f9a99d241d487
Miller Wesselhoff <millerwesselhoff@gmail.com>
2022-01-04 13:59:06 -0600

    added radio buttons to select pen type added algorithm for converting rgba to hex added algorithm for darkening and lightening color

--- script.js
+++ script.js
@@ -2,24 +2,60 @@ const container = document.getElementById("container");
 let rows = document.getElementsByClassName("row");
 let pixels = document.getElementsByClassName("pixel");
 let mouseDown = false;
-let color = "#e66465";
-let tempColor = "#e66465";
+let colorPicker = document.getElementById("pen-color");
+let color = colorPicker.value;
 let colorToggle = false;
+let lightToggle = false;
+let darkToggle = false;
+let radioButtons = document.getElementsByName("pen-style");
+const rgba2hex = (rgba) => `#${rgba.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/).slice(1).map((n, i) => (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n)).toString(16).padStart(2, '0').replace('NaN', '')).join('')}`
+
 
 defaultGrid();
 
-document.getElementById("randomize").addEventListener("click", function( event ) {
-    if(colorToggle===false) {
-        tempColor = color;
-        document.getElementById("current-toggle").innerHTML = "Random colors on";
+function checkRadioButtons() {
+    if (radioButtons[1].checked === true) {
         colorToggle = true;
-    }
-    else {
-        color = tempColor;
-        document.getElementById("current-toggle").innerHTML = "Random colors off";
+        lightToggle = false;
+        darkToggle = false;
+    } else if (radioButtons[2].checked === true) {
         colorToggle = false;
+        lightToggle = true;
+        darkToggle = false;
+    } else if (radioButtons[3].checked === true) {
+        colorToggle = false;
+        lightToggle = false;
+        darkToggle = true;
+    } else {
+        color = colorPicker.value;
+        colorToggle = false;
+        lightToggle = false;
+        darkToggle = false;
     }
-});
+}
+
+for (i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].addEventListener("change", function(event) {
+        if (radioButtons[1].checked === true) {
+            colorToggle = true;
+            lightToggle = false;
+            darkToggle = false;
+        } else if (radioButtons[2].checked === true) {
+            colorToggle = false;
+            lightToggle = true;
+            darkToggle = false;
+        } else if (radioButtons[3].checked === true) {
+            colorToggle = false;
+            lightToggle = false;
+            darkToggle = true;
+        } else {
+            color = colorPicker.value;
+            colorToggle = false;
+            lightToggle = false;
+            darkToggle = false;
+        }
+    });
+}
 
 document.getElementById("reset").addEventListener("click", function( event ) {
     for(i = 0; i < pixels.length; i++) {
@@ -31,6 +67,7 @@ function defaultGrid() {
     makeRows(16);
     makeColumns(16);
     initialize();
+    checkRadioButtons();
 }
 
 function makeRows(amount) {
@@ -68,20 +105,35 @@ container.addEventListener("mouseup", function() {
 
 function initialize() {
     for(i=0; i < pixels.length; i++) {
+        if(!pixels[i].style.backgroundColor) {
+            pixels[i].style.backgroundColor = "#FFFFFF";
+        }
         pixels[i].addEventListener("mouseover", function(event) {
             if (mouseDown===true) {
                 if(colorToggle===true) {
                     randomizeColor();
+                    event.target.style.backgroundColor = color;
+                } else if (lightToggle===true) {
+                    event.target.style.backgroundColor = LightenDarkenColor(rgba2hex(event.target.style.backgroundColor.toString()), 10);
+                } else if (darkToggle===true) {
+                    event.target.style.backgroundColor = LightenDarkenColor(rgba2hex(event.target.style.backgroundColor.toString()), -10);
+                } else {
+                    event.target.style.backgroundColor = color;
                 }
-                event.target.style.backgroundColor = color;
             }
         }, false);
 
         pixels[i].addEventListener("mousedown", function(event) {
             if(colorToggle===true) {
                 randomizeColor();
+                event.target.style.backgroundColor = color;
+            } else if (lightToggle===true) {
+                event.target.style.backgroundColor = LightenDarkenColor(rgba2hex(event.target.style.backgroundColor.toString()), 10);
+            } else if (darkToggle===true) {
+                event.target.style.backgroundColor = LightenDarkenColor(rgba2hex(event.target.style.backgroundColor.toString()), -10);
+            } else {
+                event.target.style.backgroundColor = color;
             }
-            event.target.style.backgroundColor = color;
         }, false);
 
         pixels[i].style.height = Math.floor(800/Math.sqrt(pixels.length)) - 1;
@@ -109,8 +161,20 @@ slider.addEventListener("click", function(event) {
     initialize();
 });
 
-let colorPicker = document.getElementById("pen-color");
 
 colorPicker.addEventListener("change", function(event){
     color = event.target.value;
+    tempColor = color;
 });
+
+function LightenDarkenColor(col, amt) {
+    return (
+        '#' +
+        col
+          .replace(/^#/, '')
+          .replace(/../g, (col) =>
+            ('0' + Math.min(255, Math.max(0, parseInt(col, 16) + amt)).toString(16)).substr(-2)
+          )
+      );
+  
+}
